fe7154986f35a82ba9eb63f587fcade70c83fd42
aceofknights <49144215+aceofknights@users.noreply.github.com>
2024-04-11 18:32:48 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Tabs from './Tabs/BottomTab'; // Assuming Tabs contains the bottom tab navigator
 import 'react-native-gesture-handler';
 import Auth from './Tabs/AuthStack'
-// Import your screens here
-import CreateAccountPage from './Screens/createAccountPage';
-import LoginPage from './Screens/LoginPage'; // Make sure to import LoginPage as well
 
 export default function App(){
   return (
